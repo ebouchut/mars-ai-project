55eb9f21f34f7c53a34b07ba1c4d3b66218a593f
Eric Bouchut <ebouchut@gmail.com>
2026-02-02 10:25:29 +0100

    chore(db): Add Prisma shadow database creation

--- backend/database/create-database.sql
+++ backend/database/create-database.sql
@@ -1,4 +1,15 @@
-CREATE DATABASE IF NOT EXISTS marsAI DEFAULT CHARACTER SET utf8mb4;
+-- Create the databases for the marsAI project and the database user
+-- Replace the database names and username with your current configuration in .env.
 
--- Now populate the database
--- See: schema.sql
+CREATE DATABASE IF NOT EXISTS marsai                          DEFAULT CHARACTER SET utf8mb4;
+CREATE DATABASE IF NOT EXISTS prisma_migrate_shadow_db_marsai DEFAULT CHARACTER SET utf8mb4;
+
+CREATE USER IF NOT EXISTS marsai@localhost IDENTIFIED BY 'TODO_PASSWORD_HERE';
+
+
+GRANT ALL PRIVILEGES ON marsai.*                          TO marsai@localhost;
+-- See: https://www.prisma.io/docs/orm/prisma-migrate/understanding-prisma-migrate/shadow-database
+GRANT ALL PRIVILEGES ON prisma_migrate_shadow_db_marsai.* TO marsai@localhost;
+GRANT CREATE, DROP   ON *.*                               TO marsai@localhost;
+FLUSH PRIVILEGES;
+
