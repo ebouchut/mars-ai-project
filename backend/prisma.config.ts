67a6dc8d2452d058b53d5ae129071d6a105a76e4
Eric Bouchut <ebouchut@gmail.com>
2026-02-03 16:30:57 +0100

    chore(db): Add JS file to seed the dev database

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -8,6 +8,7 @@ export default {
     schema: 'prisma/schema.prisma',
     migrations: {
         path: 'prisma/migrations',
+        seed: 'database/seed.ts'
     },
     datasource: {
         url: env('DATABASE_URL'),
